a94bdccc258011940a6f9001f5f31ee304793c89
diegocotta <diego.cotta@zup.com.br>
2018-10-16 09:55:39 -0300

    done

--- app/src/main/java/com/example/android/mygarden/provider/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/provider/PlantWidgetProvider.java
@@ -1,11 +1,14 @@
 package com.example.android.mygarden.provider;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.example.android.mygarden.R;
+import com.example.android.mygarden.ui.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -14,12 +17,13 @@ public class PlantWidgetProvider extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Create an Intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
-
-
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
